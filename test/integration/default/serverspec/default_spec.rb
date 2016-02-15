7997a47064d1ab7b7b2bab60bde0696de297b6a7
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2016-02-15 21:53:19 +0000

    [OI] Initial commit

---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+jenkins_extra_home = "/var/lib/jenkins"
+
+describe file("#{jenkins_extra_home}/.aws/credentials") do
+  it { should be_file }
+  it { should be_mode 600 }
+end
+
+describe file("#{jenkins_extra_home}/.aws/config") do
+  it { should be_file }
+  it { should be_mode 600 }
+end
+
+describe file("#{jenkins_extra_home}/create-credentials-config.py") do
+  it { should be_file }
+  it { should be_mode 755 }
+end
+
+describe file("#{jenkins_extra_home}/.ssh/infra-myawesomekey") do
+  it { should be_file }
+  it { should be_mode 600 }
+end
+
+describe file("#{jenkins_extra_home}/.ssh/infra-myawesomekey.pub") do
+  it { should be_file }
+  it { should be_mode 644 }
+end
