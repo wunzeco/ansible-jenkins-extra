ca825444f379cf7a83e01480fb25e61cfd177544
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2016-03-02 01:46:30 +0000

    [OI] Support for PEM keys added. Fix use of bare variables with "with_items".

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -26,3 +26,13 @@ describe file("#{jenkins_extra_home}/.ssh/infra-myawesomekey.pub") do
   it { should be_file }
   it { should be_mode 644 }
 end
+
+describe file("#{jenkins_extra_home}/.ssh/infra-anotherkey.pem") do
+  it { should be_file }
+  it { should be_mode 600 }
+end
+
+describe file("#{jenkins_extra_home}/.ssh/infra-anotherkey.pub") do
+  it { should be_file }
+  it { should be_mode 644 }
+end
